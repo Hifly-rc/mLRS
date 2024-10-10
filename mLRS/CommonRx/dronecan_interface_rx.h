da853ad4f6445de60b76f588b4cc281ba28baf60
Hifly-rc <120536716+Hifly-rc@users.noreply.github.com>
2024-10-10 18:09:14 +0800

    update modules

--- mLRS/CommonRx/dronecan_interface_rx.h
+++ mLRS/CommonRx/dronecan_interface_rx.h
@@ -26,7 +26,10 @@
 #error FDCAN_IRQ_PRIORITY not eq DRONECAN_IRQ_PRIORITY !
 #endif
 
+<<<<<<< HEAD
+=======
 extern tRxMavlink mavlink;
+>>>>>>> 6a758033410c79bc344e6a867e9dc0d71d2c5f17
 extern tRxDroneCan dronecan;
 
 extern uint16_t micros16(void);
@@ -343,6 +346,11 @@ void tRxDroneCan::SendRcData(tRcData* const rc_out, bool failsafe)
     // this message's quality is used by ArduPilot for setting rssi (not LQ)
     // it goes from 0 ... 255
     // so we use the same conversion as in e.g. RADIO_STATUS, so that ArduPilot shows us (nearly) the same value
+<<<<<<< HEAD
+    _p.rc_input.quality = (connected()) ? rssi_i8_to_ap(stats.GetLastRssi()) : 0;
+    if (connected()) {
+        _p.rc_input.status |= DRONECAN_SENSORS_RC_RCINPUT_STATUS_QUALITY_VALID;
+=======
 
 #define DRONECAN_SENSORS_RC_RCINPUT_STATUS_QUALITY_VALID 1
 
@@ -372,6 +380,7 @@ void tRxDroneCan::SendRcData(tRcData* const rc_out, bool failsafe)
             _p.rc_input.quality = rssi_i8_to_ap(stats.GetLastRssi());
             _p.rc_input.status |= DRONECAN_SENSORS_RC_RCINPUT_STATUS_QUALITY_VALID;
         }
+>>>>>>> 6a758033410c79bc344e6a867e9dc0d71d2c5f17
     }
 
     _p.rc_input.rcin.len = 16;
